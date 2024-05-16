e683b0c0a908fd7acddd4c0a1ebcc8c3b3e7c5af
pongudao <168678665+pongudao@users.noreply.github.com>
2024-05-15 23:36:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -42,11 +42,11 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
-    for (let i=0; i<contadores.length;i++){
-        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
+        for (let i=0; i<contadores.lenght;i++){ 
+            document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+            document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+            document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2]; 
+            document.getElementById("seg"+1).textContent = calculaTempo(tempos[i])[3];
     }
 }
 
